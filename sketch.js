d22dcbdb4e865349145a98dbaabe21294f1c5dfb
AayushBindal <73348182+AayushBindal@users.noreply.github.com>
2021-08-09 19:26:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,17 +17,11 @@ async function chat() {
   let txt = textInput.value();
   reply = await bot.reply('local-user', txt);
   searchValue = textInput.value();
-  if(navigator.userAgent.toLowerCase().match(/mobile/i)) {
-     console.log("not a computer");
-  }
-  else{
   var s = createP(searchValue);
   s.addClass("userchat");
   speech.speak(reply);
   botch = createP(reply).addClass('botchat');
   createA("https://www.google.com/search?q="+searchValue+"&cvid=ba194626fbac4114be33953f19549c7a&aqs=edge.0.0l7.579j0j1&pglt=931&FORM=ANNTA1&PC=HCTS","my I search it for you","_blank");
-    console.log("A computer");
-}
   
   botch.id="botchatId";
   console.log(botch.id);
